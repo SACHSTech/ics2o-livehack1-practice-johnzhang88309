464f69e5697dda970d31d338acd0f6705a7a9ece
johnzhang88309 <john.zhang23@ycdsbk12.ca>
2021-02-09 14:57:33 +0000

    Visuals

--- days_hours.py
+++ days_hours.py
@@ -1,15 +1,15 @@
 """
-
+--------------------------------------------------------
 Name: Hours to days and hours.py
 Purpose: Convert hours to days and hours
 
 Author: Zhang.J
 
 Created: 08/02/2021
-
+--------------------------------------------------------
 """
 
-#find out number of hours 
+#find out number of hours from the user
 hours = int(input("Enter the amount of hours: "))
 
 #convert hours to days and hours 
@@ -17,5 +17,6 @@ days = (hours//24)
 remaining_hours = hours%24
 
 print("")
+
 #print the days and hours together 
 print ("The total days and hours is " + (str(days)) + " days " + (str(remaining_hours)) + " hours")
--- f_to_c.py
+++ f_to_c.py
@@ -1,18 +1,18 @@
 """
-
+--------------------------------------------------------
 Name: Farenheit to celcius.py
 Purpose: Convert farenheit to celcius 
 
 Author: Zhang.J
 
 Created: 08/02/2021
-
+--------------------------------------------------------
 """
 
-#get the degrees in farenheit
+#get the degrees in farenheit from the user
 degrees_farenheit = float(input("Enter the degrees in farenheit: "))
 
-#convert farenheit to celcius
+#convert farenheit to celcius with the formula
 degrees_celcius = (degrees_farenheit-32) * (5/9)
 
 print("")
